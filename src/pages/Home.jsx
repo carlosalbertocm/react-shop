be3923e0ef81a815e72f2d167ddb14ec19c7e422
Carlos Alberto Conde Moreno <carlosalbertocondemoreno@gmail.com>
2022-07-28 17:47:18 -0500

    Creacion del header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
-	return <h1>Home</h1>;
+	return (
+		<>
+			<Header />
+		</>
+	);
 };
 
 export default Home;
