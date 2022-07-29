8959b326360d19ea58f6b0cb9377f126a1572ab7
Carlos Alberto Conde Moreno <carlosalbertocondemoreno@gmail.com>
2022-07-29 10:10:06 -0500

    hook useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,33 @@
-import React from "react";
-import "../styles/Login.scss";
+import React, { useRef } from "react";
+import "@styles/Login.scss";
+import logo from "@assets/logos/logo_yard_sale.svg";
 
 const Login = () => {
+	const form = useRef(null);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData(form.current);
+		const data = {
+			username: formData.get("email"),
+			password: formData.get("password"),
+		};
+		console.log(data);
+	};
+
 	return (
 		<div className="Login">
 			<div className="Login-container">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-				<form action="/" className="form">
+				<img src={logo} alt="logo" className="logo" />
+				<form action="/" className="form" ref={form} onSubmit={handleSubmit}>
 					<label htmlFor="email" className="label">
 						Email address
 					</label>
 					<input
 						type="text"
 						id="email"
-						placeholder="platzi@example.cm"
+						name="email"
+						placeholder="me@carlosalbertocm.com"
 						className="input input-email"
 					/>
 					<label htmlFor="password" className="label">
@@ -22,6 +36,7 @@ const Login = () => {
 					<input
 						type="password"
 						id="password"
+						name="password"
 						placeholder="*********"
 						className="input input-password"
 					/>
